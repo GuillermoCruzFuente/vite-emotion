f49267e7bb66c2fee4fa7690b242fe93fe100697
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-10-20 23:29:41 -0500

    dev server link fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import dns from 'dns'
+dns.setDefaultResultOrder('verbatim')
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
@@ -11,4 +14,10 @@ export default defineConfig({
 			},
 		}),
 	],
+	server: {
+		port: 5400,
+		strictPort: true,
+		host: 'localhost',
+		open: true,
+	},
 })

7eb0e516cef27c3479abf67a1f0aba8a730b83ab
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-10-22 23:54:18 -0500

    Chip component styles added

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,3 +1,22 @@
-const Chip = () => {}
+import { css } from '@emotion/react'
+
+type ChipType = {
+	children: string
+}
+
+const chipStyle = css({
+	backgroundColor: '#210B38',
+	color: '#C3A1E8',
+	width: 'fit-content',
+	padding: '0.25rem 0.5rem',
+	fontWeight: '900',
+	fontSize: '0.55rem',
+	borderRadius: '1rem',
+	boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.15)',
+})
+
+const Chip = ({ children }: ChipType) => {
+	return <div css={chipStyle}>{children}</div>
+}
 
 export default Chip
